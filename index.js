c0388c5c615958e8fe1fd916627969629d197d0f
Ian <lsouza.ian@gmail.com>
2021-05-27 22:08:42 -0300

    http requests

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const port = 3000;
 
 const usuarioIan = {
-  id: 1,
+  id: 0,
   nome: "ian",
   idade: 29,
 };
@@ -17,6 +17,41 @@ app.get("/usuarios", (req, res) => {
   res.send(JSON.stringify(usuarios));
 });
 
+app.post("/usuarios", (req, res) => {
+  const { nome, idade } = req.body;
+  const id = usuarios.length;
+
+  const novoUsuario = {
+    id,
+    nome,
+    idade,
+  };
+
+  usuarios.push(novoUsuario);
+  res.json(novoUsuario);
+});
+
+app.put("/usuarios/:id", (req, res) => { // : = nome do parametro
+  const { id } = req.params;
+  const { nome, idade } = req.body;
+
+  const usuario = usuarios.find(usuario => usuario.id === Number(id));
+  usuario.nome = nome;
+  usuario.idade = idade;
+
+  res.json(usuario);
+});
+
+app.delete("/usuarios/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = usuarios.findIndex((usuario = usuario.id === Number(id)));
+
+  usuarios.splice(index, 1);
+
+  res.status(204).send();
+});
+
 app.listen(port, () => {
-  console.log(`Server is up and running on port ${3000}`);
+  console.log(`Server is up and running on port ${port}`);
 });
